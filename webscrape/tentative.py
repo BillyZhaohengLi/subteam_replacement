b6693fb238a7b03b7da8f80da8299ade8de3826f
BillyZhaohengLi <31910858+BillyZhaohengLi@users.noreply.github.com>
2021-02-16 23:53:32 -0600

    Add files via upload

--- webscrape/tentative.py
+++ webscrape/tentative.py
@@ -55,6 +55,7 @@ def extract_skill_ids(skill_html):
 """
 def extract_skill_endorsers(url, cookies):
     r = requests.get(url, cookies=cookies).text.replace("&quot;", '"') 
+    sleep(np.random.rand(1, 2) * 10)
     data_infos = re.findall(r'({"data":.*?)\n', r) 
     endorser_infos = []
     skill_name = "" 
@@ -136,15 +137,17 @@ def single_iteration(user_id):
     skill_suffix = "detail/skills/" 
 
     # Get cookie for session infomation to be used for requests
-    cookie_str = 'li_sugr=2b62d20d-4905-444e-a538-f07a1feadb2f; bcookie="v=2&6f8410b8-b0d8-4b8f-8bf5-239142321e88"; lissc=1; bscookie="v=1&20201220200242e344c232-aa24-4519-8a27-d5da6b9506ceAQH71xyZxpNwSM1qSRHN1FShAdXZzBD-"; li_rm=AQGOQGh5fUe25wAAAXcdkjMFAjYe0or_97PLjw2NGfZh6pu0Tov0J624hwaTwTGnjhvFTXD9KU6MPwfRYE-kkKryJcGqwq1vjP1BH94G5dTNVUcibTpgw2Nb; _ga=GA1.2.388260073.1611108862; aam_uuid=17193061939447905092347891705167064179; g_state={"i_l":0}; JSESSIONID="ajax:6661573881781652824"; _gcl_au=1.1.1133925316.1612394929; timezone=America/Guatemala; _gid=GA1.2.361636778.1612997530; lang=v=2&lang=en-us; spectroscopyId=f25b1ae3-4235-432c-9b9a-d5ddd91c095b; AMCVS_14215E3D5995C57C0A495C55@AdobeOrg=1; li_oatml=AQHphKFmQsnfEAAAAXedJEu6wjMdOwdTn-Xo3WlQNwaLxaY0wLA5Bh-vWpXud-JMRpNS7BlbZIhhMVJMytgFQzwvIEz2Brjr; AMCV_14215E3D5995C57C0A495C55@AdobeOrg=-637568504|MCIDTS|18673|MCMID|16627044507729075582331259510278283192|MCAAMLH-1613877811|7|MCAAMB-1613877811|RKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y|MCOPTOUT-1613280211s|NONE|vVersion|5.1.1|MCCIDH|-1705543759; UserMatchHistory=AQLicC0nuaE7rgAAAXefa7Fayxz6cH_kGA7mEHwKnRuSZL2JGKBXTIqNG7GK-iDszvQPUl04t0MqkV9fxs2QOznTs5K4NO0dPAYwMriMHVb3JUnvtv8TRxYPPjDUv_LKK3e6hMzMTy_qEWvJ2ZOrGCPnV2wEJn83mGlV1zOE3AWzJjXja-YKJDQLWXL6lsvIL4RPy-ypX9wjJXRu7Nmb9LoOFfaSOM3q3AQItOVUrNBOmfb-GID646eWolacbN9y9ikMMved3YOhEjDbNTXqZZDvk-NA-OhqurzR; lidc="b=VGST03:s=V:r=V:g=2239:u=1:i=1613290014:t=1613376414:v=1:sig=AQGHWXAzX4PXogDpnVtLT5FCEHQdY-FL"; fid=AQEkbzxtR1iepwAAAXefmB0ypLJAi1e1kLk56w0gbG1UHY5PYEj7xG87qnYO5QwxoZukKnr4hZag4g; ccookie=AQG/hou2WWLtzQAAAXefmIWIz6Jr0kzj0JkXz6wiMEPuC3oIU6PluhUpjd5460WFnjJbQA0pkW/Fd3Ob8ufBRwVyKHfmo+A/Etmek0er0tDWIs0uhi2t56AVIz/bIoPJTCiX9Q==; G_ENABLED_IDPS=google; fid=AQHuQUOQ04JdWgAAAXefnPqVfRN0QzZwaPi4NRVPh7Uib_sTJlKBSfdW5IiBDFIAy-bjnTKSMv_7BQ; _gat=1; fcookie=AQGGgMfVcEi5RwAAAXefnQaVyw9vSTFWS8Hh3TDLkIVInO-ey0q6Bv0_M7mk4ceN5Kr9aaylorBsyvgKBVfgnGhWoGVzOFHqu1aVSM0T6U7fZ5pGVdphZ8Cf8x_FOoBkeEaOobVnNIFGRb-9zl3CyH6WYGZ1cQP_ihXKJZ-CJf5iUda0QIH_lAsAjVW4pmTWiD1UJxQ1aZIxntG9PqCPu8pBzAJ0XJrb9kM_U2aLDNqhj2EtV_Mm65blOUWv2DZ8129C-vxl_XgC6JrNEIT2esdJG0kZ4k1Enrj4x/rj8kbyVRjhMOspl+PRbI+TGnVKK7x7kIfzVsWuWByFPUBZBCuHArYR7RaHkYGWdA=='
+    cookie_str = 'bcookie="v=2&1f78842f-a151-4360-8ac2-31f1bbc2d39b"; bscookie="v=1&2021021704315186a72ec2-bc60-4326-814a-003b829d856eAQHz4JoFtIkUkWyrE6aXQTbBuijcFLKy"; lidc="b=TGST07:s=T:r=T:g=2004:u=1:i=1613540528:t=1613626784:v=1:sig=AQEha5_Zjt6j6vmTHmE_oCjUdoVmCfl5"; _ga=GA1.2.1559737382.1613536312; _gid=GA1.2.1694167689.1613536312; AMCV_14215E3D5995C57C0A495C55%40AdobeOrg=-637568504%7CMCIDTS%7C18676%7CMCMID%7C78801844199195096893114186480001794538%7CMCAAMLH-1614145186%7C7%7CMCAAMB-1614145186%7C6G1ynYcLPuiQxYZrsz_pkqfLG9yMXBpb2zX5dvJdYQJzPXImdj0y%7CMCOPTOUT-1613547586s%7CNONE%7CvVersion%7C5.1.1%7CMCCIDH%7C-2008823391; AMCVS_14215E3D5995C57C0A495C55%40AdobeOrg=1; aam_uuid=78956972041796514423168562877135467041; li_rm=AQHOGTJv25i5EAAAAXeuQkE6sw6sztgq5NHS8USlTobC1lm3PCTpguxLH91PeK2JwD-7GIPYGRNeW5k-l44E-J7n9i5zTPyYciJ_joSk6rFAuDf6ZMf8H49f; timezone=America/Chicago; UserMatchHistory=AQIKb89i5IMeQgAAAXeugouBtLRnOU9o85f5i9tKAIA3HoxcgnIDolbRB9VJpaC4N_y8xFRgHLWnuzoXvbU-lHV7ON-q4Z3CXpizHpLjvdrk2xUK2-SCqn21Q8oEJdBqpPfwvnyF87Tn_yl4JBMYt0PliZOr2d9DWyMtZwd3exwI-aQt4xEFH8vr2paBrA7lygjzVdD0MIy6SbUxW7nC2QKApJoKe7_kbeQ10DqreZgsaFS9sUL6r_ptufGV8R4oFigAasRqmZs9p6-SByuG8KPxiRebIFv7fCaThVg; li_sugr=e35415c5-b8a3-4b3b-a9fb-da8f4638e74e; AnalyticsSyncHistory=AQKpJM41wtiWCQAAAXeuRPalcGiTNduse-pD3v8RurQ7jQFvCwZylLBnttYbVPGeJDqWNZ22VExTFPDVaw27gQ; lms_ads=AQEx79nPATwCzAAAAXeuRPf_uj0qBtR0Taknh4EBQxIOgQNkRTQ4X4trhLwkcmCN3hXVyBozzGTaEzxeaac1mzvDPt5yGLmB; lms_analytics=AQEx79nPATwCzAAAAXeuRPf_uj0qBtR0Taknh4EBQxIOgQNkRTQ4X4trhLwkcmCN3hXVyBozzGTaEzxeaac1mzvDPt5yGLmB; visit=v=1&M; lang=v=2&lang=en-us; JSESSIONID=ajax:7755993694139518288; li_at=AQEDATR6hEwFUpVsAAABd66AXnUAAAF30ozidU4AVqQxUbooUQudhzPMpHtSo4CPS3uxvFjEVWgoY87AtoZVJCzgFb0nc11wC-nmz5yHyFiv0k-FhXOTe3055NI_rORgHl4-0OeXNfsdF5sRJjUJsBz5; liap=true; sdsc=22%3A1%2C1613540434448%7EJBSK%2C01We5iEWmvgqYjBgb0R1NgEsVPGQ%3D; _gcl_au=1.1.1504729621.1613540440'
     cookies = cookie_parser(cookie_str) 
     
     # Extract user experience from user_base_url
     src_html = requests.get(user_base_url, cookies=cookies).text  
+    sleep(np.rand(1, 2) * 5)
     experiences = extract_experience(src_html) 
     
     # Extract all skill_ids
     skill_src_html = requests.get(user_base_url + skill_suffix, cookies=cookies).text  
+    sleep(np.rand(1, 2) * 5)
     skill_ids = extract_skill_ids(skill_src_html)    
     
     # Based on skill_ids, construct all requests(skill pages) to be queried
@@ -170,10 +173,13 @@ if __name__ == "__main__":
     visited_users = set() 
     user2info = {} 
     count = 0
+    failure_count = 0
 
     while(len(queue) != 0):  
         top_id = queue.pop(0) 
         if top_id in visited_users: continue
+        if failure_count > 10:
+            break
         
         visited_users.add(top_id)
 
@@ -183,14 +189,30 @@ if __name__ == "__main__":
             for l in skill2endorsers.values(): 
                 for user in l:            
                     queue.append(user)
+
+            with open('queue.pkl', 'r') as fp:
+                data = fp.read()
+            with open('queue.pkl', 'wb') as fp:
+                pickle.dump(queue, fp, protocol=pickle.HIGHEST_PROTOCOL)
+
+            with open('visited_users.pkl', 'r') as fp:
+                data = fp.read()
+            with open('visited_users.pkl', 'wb') as fp:
+                pickle.dump(visited_users, fp, protocol=pickle.HIGHEST_PROTOCOL)
+
+            failure_count = 0
+            sleep(np.random.gamma(shape = 10.0, scale = 1.0))
+
         except:
             print(f"Currently successfully recorded {count} users, failed on {len(visited_users) - count} users...") 
+            failure_count += 1
+            sleep(np.random.gamma(shape = 10.0, scale = 1.0))
             continue 
             
         count += 1 
         print(f"Currently successfully recorded {count} users, failed on {len(visited_users) - count} users...") 
         if len(visited_users) % 20 == 0: sleep(60)  
-        if (count >= 100): break  
+        if (count >= 1000): break  
         
 
     with open('user2info.pkl', 'wb') as fp:
